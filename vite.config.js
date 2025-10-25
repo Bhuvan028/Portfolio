12d1b76aaa37c5e33d2fa0357f5aeb90c514dfeb
Bhuvan <pasapalabhuvanesh028@gmail.com>
2025-10-25 16:30:30 +0530

    added deploy file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
